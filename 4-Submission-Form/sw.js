345d51715e6e70eeba7bbba42e891370af0dcd0d
kubstar <52975077+kubstar@users.noreply.github.com>
2021-01-15 00:01:01 +0530

    submission form --indexDB --init(1)

--- 4-Submission-Form/sw.js
+++ 4-Submission-Form/sw.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log('WORKER: executing.');
-var version = 'v1';
+
+var version = 'v1::';
 var offlineFundamentals = [
     '/4-Submission-Form/index.html',
     '/4-Submission-Form/icons/favicon.ico',
@@ -8,88 +9,119 @@ var offlineFundamentals = [
     '/4-Submission-Form/css/style.css',
     '/4-Submission-Form/fonts/material-icon/css/material-design-iconic-font.min.css'
 ];
-self.addEventListener("install", function (event) {
-    console.log('WORKER: install event in progress.');
-    event.waitUntil(
-        caches
-        .open(version + 'fundamentals')
-        .then(function (cache) {
-            return cache.addAll(offlineFundamentals);
-        })
-        .then(function () {
-            console.log('WORKER: install completed');
-        })
-    );
+
+self.addEventListener("install", function(event) {
+  console.log('WORKER: install event in progress.');
+  event.waitUntil(caches.open(version + 'fundamentals').then(function(cache) {
+        return cache.addAll(offlineFundamentals);
+      }).then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
 });
-self.addEventListener("fetch", function (event) {
-    console.log('WORKER: fetch event in progress.');
-    if (event.request.method !== 'GET') {
 
-        console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-        return;
-    }
+self.addEventListener("fetch", function(event) {
+  console.log('WORKER: fetch event in progress.');
+  if (event.request.method !== 'GET') {
+    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+    return;
+  }
 
-    event.respondWith(
-        caches
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+        var networked = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        return cached || networked;
 
-        .match(event.request)
-        .then(function (cached) {
-            var networked = fetch(event.request)
+        function fetchedFromNetwork(response) {
+          var cacheCopy = response.clone();
 
-                .then(fetchedFromNetwork, unableToResolve)
-                .catch(unableToResolve);
-            console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-            return cached || networked;
+          console.log('WORKER: fetch response from network.', event.request.url);
 
-            function fetchedFromNetwork(response) {
+          caches
+            // We open a cache to store the response for this request.
+            .open(version + 'pages')
+            .then(function add(cache) {
+              /* We store the response for this request. It'll later become
+                 available to caches.match(event.request) calls, when looking
+                 for cached responses.
+              */
+              cache.put(event.request, cacheCopy);
+            })
+            .then(function() {
+              console.log('WORKER: fetch response stored in cache.', event.request.url);
+            });
 
-                var cacheCopy = response.clone();
+          // Return the response so that the promise is settled in fulfillment.
+          return response;
+        }
 
-                console.log('WORKER: fetch response from network.', event.request.url);
+        /* When this method is called, it means we were unable to produce a response
+           from either the cache or the network. This is our opportunity to produce
+           a meaningful response even when all else fails. It's the last chance, so
+           you probably want to display a "Service Unavailable" view or a generic
+           error response.
+        */
+        function unableToResolve () {
+          /* There's a couple of things we can do here.
+             - Test the Accept header and then return one of the `offlineFundamentals`
+               e.g: `return caches.match('/some/cached/image.png')`
+             - You should also consider the origin. It's easier to decide what
+               "unavailable" means for requests against your origins than for requests
+               against a third party, such as an ad provider.
+             - Generate a Response programmaticaly, as shown below, and return that.
+          */
 
-                caches
+          console.log('WORKER: fetch request failed in both cache and network.');
 
-                    .open(version + 'pages')
-                    .then(function add(cache) {
-                        cache.put(event.request, cacheCopy);
-                    })
-                    .then(function () {
-                        console.log('WORKER: fetch response stored in cache.', event.request.url);
-                    });
-                return response;
-            }
-            function unableToResolve() {
-                console.log('WORKER: fetch request failed in both cache and network.');
-                return new Response('<h1>Service Unavailable</h1>', {
-                    status: 503,
-                    statusText: 'Service Unavailable',
-                    headers: new Headers({
-                        'Content-Type': 'text/html'
-                    })
-                });
-            }
-        })
-    );
+          /* Here we're creating a response programmatically. The first parameter is the
+             response body, and the second one defines the options for the response.
+          */
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          });
+        }
+      })
+  );
 });
-self.addEventListener("activate", function (event) {
-    console.log('WORKER: activate event in progress.');
-    event.waitUntil(
-        caches
-        .keys()
-        .then(function (keys) {
 
-            return Promise.all(
-                keys
-                .filter(function (key) {
-                    return !key.startsWith(version);
-                })
-                .map(function (key) {
-                    return caches.delete(key);
-                })
-            );
-        })
-        .then(function () {
-            console.log('WORKER: activate completed.');
-        })
-    );
+/* The activate event fires after a service worker has been successfully installed.
+   It is most useful when phasing out an older version of a service worker, as at
+   this point you know that the new worker was installed correctly. In this example,
+   we delete old caches that don't match the version in the worker we just finished
+   installing.
+*/
+self.addEventListener("activate", function(event) {
+  /* Just like with the install event, event.waitUntil blocks activate on a promise.
+     Activation will fail unless the promise is fulfilled.
+  */
+  console.log('WORKER: activate event in progress.');
+
+  event.waitUntil(
+    caches
+      /* This method returns a promise which will resolve to an array of available
+         cache keys.
+      */
+      .keys()
+      .then(function (keys) {
+        // We return a promise that settles when all outdated caches are deleted.
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              // Filter by keys that don't start with the latest version prefix.
+              return !key.startsWith(version);
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function() {
+        console.log('WORKER: activate completed.');
+      })
+  );
 });
